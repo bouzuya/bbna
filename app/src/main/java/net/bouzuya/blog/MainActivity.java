1dbee6f8de861b4251a2748b387bd650560ebe1f
bouzuya <m@bouzuya.net>
2017-04-10 08:19:37 +0900

    add MainActivity#newEntries()

--- app/src/main/java/net/bouzuya/blog/MainActivity.java
+++ app/src/main/java/net/bouzuya/blog/MainActivity.java
@@ -9,5 +9,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Entry> entryList = newEntries();
+    }
+
+    @NonNull
+    private List<Entry> newEntries() {
+        List<Entry> entryList = new ArrayList<>();
+        entryList.add(new Entry("2017-01-01", "entry 1"));
+        entryList.add(new Entry("2017-01-02", "entry 2"));
+        entryList.add(new Entry("2017-01-03", "entry 3"));
+        return entryList;
     }
 }
