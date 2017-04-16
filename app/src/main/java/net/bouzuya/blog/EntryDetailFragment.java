09fef2c250e85c2d5db82f0cce3817a7851bbbc4
bouzuya <m@bouzuya.net>
2017-04-16 19:24:13 +0900

    add EntryDetailFragment

---
+++ app/src/main/java/net/bouzuya/blog/EntryDetailFragment.java
@@ -0,0 +1,141 @@
+package net.bouzuya.blog;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+import net.bouzuya.blog.loaders.EntryDetailLoader;
+import net.bouzuya.blog.models.EntryDetail;
+import net.bouzuya.blog.models.Result;
+
+public class EntryDetailFragment extends Fragment {
+    public static final int ENTRY_DETAIL_LOADER_ID = 1;
+
+    private static final String DATE = "param1";
+    private static final String TAG = EntryDetailFragment.class.getSimpleName();
+
+    private String mDate;
+
+    public EntryDetailFragment() {
+        // Required empty public constructor
+    }
+
+    public static EntryDetailFragment newInstance(String date) {
+        EntryDetailFragment fragment = new EntryDetailFragment();
+        Bundle arguments = new Bundle();
+        arguments.putString(DATE, date);
+        fragment.setArguments(arguments);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle arguments = getArguments();
+        if (arguments != null) {
+            mDate = arguments.getString(DATE);
+            initEntryDetailLoader(mDate);
+        } else {
+            initEntryDetailLoader(null);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_entry_detail, container, false);
+    }
+
+    private void initEntryDetailLoader(String dateOrNull) {
+        LoaderManager loaderManager = getLoaderManager();
+        Bundle bundle = new Bundle();
+        bundle.putString("date", dateOrNull);
+        LoaderManager.LoaderCallbacks<Result<EntryDetail>> callbacks =
+                new LoaderManager.LoaderCallbacks<Result<EntryDetail>>() {
+                    @Override
+                    public Loader<Result<EntryDetail>> onCreateLoader(int id, Bundle args) {
+                        if (id != ENTRY_DETAIL_LOADER_ID) throw new AssertionError();
+                        String dateOrNull = args.getString("date");
+                        return new EntryDetailLoader(
+                                EntryDetailFragment.this.getContext(), dateOrNull
+                        );
+                    }
+
+                    @Override
+                    public void onLoadFinished(
+                            Loader<Result<EntryDetail>> loader,
+                            Result<EntryDetail> data
+                    ) {
+                        onLoadEntryDetailFinished(data);
+                    }
+
+                    @Override
+                    public void onLoaderReset(Loader<Result<EntryDetail>> loader) {
+                        // do nothing
+                    }
+                };
+        loaderManager.restartLoader(ENTRY_DETAIL_LOADER_ID, bundle, callbacks);
+    }
+
+
+    private void onLoadEntryDetailFinished(Result<EntryDetail> data) {
+        Log.d(TAG, "onLoadEntryDetailFinished: ");
+        if (data.isOk()) {
+            Log.d(TAG, "onLoadEntryDetailFinished: isOk");
+
+            initEntryDetailLoader(null);
+
+            EntryDetail newEntryDetail = data.getValue();
+            String message = "load " + newEntryDetail.getDate() + "";
+            Toast.makeText(this.getContext(), message, Toast.LENGTH_LONG).show();
+            EntryDetail d = newEntryDetail;
+
+            String html = new StringBuilder()
+                    .append("<html>")
+                    .append("<head></head>")
+                    .append("<body>")
+                    .append("<article>")
+                    .append("<header>")
+                    .append("<h1 class=\"title\">").append(d.getTitle()).append("</h1>")
+//                    .append("<p class=\"pubdate\">").append(d.getPubdate()).append("</p>")
+//                    .append("<p class=\"minutes\">").append(d.getMinutes()).append("</p>")
+//                    .append(d.getTags().isEmpty() ? "" : "<ul class=\"tags\"><li>")
+//                    .append(this.join("</li><li>", d.getTags()))
+//                    .append(d.getTags().isEmpty() ? "" : "</li></ul>")
+                    .append("</header>")
+                    .append("<div class=\"body\">").append(d.getHtml()).append("</div>")
+                    .append("</article>")
+                    .append("</body>")
+                    .append("</html>")
+                    .toString();
+
+            View view = getView();
+            WebView webView = (WebView) view.findViewById(R.id.entry_detail);
+            webView.loadData(html, "text/html; charset=UTF-8", "UTF-8");
+        } else {
+            Exception e = data.getException();
+            Log.e(TAG, "onLoadEntryDetailFinished: ", e);
+            String message = "load error";
+            Toast.makeText(this.getContext(), message, Toast.LENGTH_LONG).show();
+        }
+    }
+
+//    private String join(String delimiter, List<String> list) {
+//        if (list.isEmpty()) return "";
+//        StringBuilder builder = new StringBuilder();
+//        for (String item : list) {
+//            builder.append(delimiter).append(item);
+//        }
+//        return delimiter.isEmpty()
+//                ? builder.toString()
+//                : builder.substring(delimiter.length()).toString();
+//    }
+}
