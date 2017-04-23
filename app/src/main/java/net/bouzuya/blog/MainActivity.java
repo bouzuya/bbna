f4b471a11d63c1bffd81836e799eb0ff783a3cad
bouzuya <m@bouzuya.net>
2017-04-23 17:46:54 +0900

    add ViewPager

--- app/src/main/java/net/bouzuya/blog/MainActivity.java
+++ app/src/main/java/net/bouzuya/blog/MainActivity.java
@@ -2,22 +2,26 @@ package net.bouzuya.blog;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+
+import net.bouzuya.blog.adapters.EntryFragmentPagerAdapter;
 
 public class MainActivity extends AppCompatActivity
         implements EntryListFragment.OnEntrySelectListener {
 
+    private static final int POSITION_LIST = 0;
+    private static final int POSITION_DETAIL = 1;
     private static final String TAG = MainActivity.class.getSimpleName();
+    private EntryFragmentPagerAdapter mAdapter;
+    private ViewPager mViewPager;
 
     @Override
     public void onEntrySelect(String date) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager
-                .beginTransaction()
-                .replace(R.id.main, EntryDetailFragment.newInstance(date))
-                .addToBackStack(null)
-                .commit();
+        mAdapter.setDetailDate(date);
+        mAdapter.notifyDataSetChanged();
+        mViewPager.setCurrentItem(POSITION_DETAIL);
     }
 
     @Override
@@ -26,9 +30,17 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager
-                .beginTransaction()
-                .add(R.id.main, EntryListFragment.newInstance())
-                .commit();
+        mViewPager = (ViewPager) findViewById(R.id.view_pager);
+        mAdapter = new EntryFragmentPagerAdapter(fragmentManager);
+        mViewPager.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mViewPager != null && mViewPager.getCurrentItem() == POSITION_DETAIL) {
+            mViewPager.setCurrentItem(POSITION_LIST);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
---
+++ app/src/main/java/net/bouzuya/blog/adapters/EntryFragmentPagerAdapter.java
@@ -0,0 +1,76 @@
+package net.bouzuya.blog.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.util.Log;
+
+import net.bouzuya.blog.EntryDetailFragment;
+import net.bouzuya.blog.EntryListFragment;
+
+public class EntryFragmentPagerAdapter extends FragmentPagerAdapter {
+    private static final int POSITION_LIST = 0;
+    private static final int POSITION_DETAIL = 1;
+
+    private String mDate;
+
+    public EntryFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+        mDate = null;
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case POSITION_LIST:
+                return EntryListFragment.newInstance();
+            case POSITION_DETAIL:
+                return EntryDetailFragment.newInstance(mDate);
+            default:
+                throw new AssertionError();
+        }
+    }
+
+
+    @Override
+    public long getItemId(int position) {
+        if (position == 0) {
+            return 0;
+        } else if (position == 1) {
+            return mDate == null
+                    ? 1
+                    : entryDateToItemId(mDate);
+        } else {
+            throw new AssertionError();
+        }
+    }
+
+    @Override
+    public int getItemPosition(Object object) {
+        if (object instanceof EntryDetailFragment) {
+            EntryDetailFragment fragment = (EntryDetailFragment) object;
+            return mDate != null && mDate.equals(fragment.getDate())
+                    ? POSITION_UNCHANGED
+                    : POSITION_NONE;
+        }
+        return super.getItemPosition(object);
+    }
+
+
+    public void setDetailDate(String date) {
+        mDate = date;
+    }
+
+    private long entryDateToItemId(String date) {
+        String[] ymdStrings = date.split("-");
+        int y = Integer.parseInt(ymdStrings[0]);
+        int m = Integer.parseInt(ymdStrings[1]);
+        int d = Integer.parseInt(ymdStrings[2]);
+        return y * 10000 + m * 100 + d;
+    }
+}
