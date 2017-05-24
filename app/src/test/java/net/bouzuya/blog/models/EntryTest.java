cc41a142f1eb7534613537b0b2249fdae686163d
bouzuya <m@bouzuya.net>
2017-05-24 09:24:39 +0900

    add EntryTest

---
+++ app/src/test/java/net/bouzuya/blog/models/EntryTest.java
@@ -0,0 +1,18 @@
+package net.bouzuya.blog.models;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+public class EntryTest {
+    @Test
+    public void simple() throws Exception {
+        Entry entry = new Entry("2006-02-03", "title 1");
+        assertThat(entry.getDate(), is(equalTo("2006-02-03")));
+        assertThat(entry.getTitle(), is(equalTo("title 1")));
+    }
+}
