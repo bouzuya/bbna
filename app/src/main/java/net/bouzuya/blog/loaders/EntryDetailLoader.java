3154adf01ec615849cd61ee12a4cb04e55ee8b26
bouzuya <m@bouzuya.net>
2017-06-04 15:32:19 +0900

    extract EntryDetailParser

--- app/src/main/java/net/bouzuya/blog/loaders/EntryDetailLoader.java
+++ app/src/main/java/net/bouzuya/blog/loaders/EntryDetailLoader.java
@@ -6,6 +6,7 @@ import android.support.v4.content.AsyncTaskLoader;
 
 import net.bouzuya.blog.models.EntryDetail;
 import net.bouzuya.blog.models.Result;
+import net.bouzuya.blog.parsers.EntryDetailParser;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -23,11 +24,13 @@ import java.util.List;
 
 public class EntryDetailLoader extends AsyncTaskLoader<Result<EntryDetail>> {
     private final String mDateOrNull;
+    private final EntryDetailParser mParser;
     private EntryDetail mEntryDetail;
 
     public EntryDetailLoader(Context context, String dateOrNull) {
         super(context);
         mDateOrNull = dateOrNull;
+        mParser = new EntryDetailParser();
     }
 
     @Override
@@ -42,7 +45,7 @@ public class EntryDetailLoader extends AsyncTaskLoader<Result<EntryDetail>> {
         try {
             URL entryJsonUrl = newEntryJsonUrl(mDateOrNull);
             String jsonString = fetch(entryJsonUrl);
-            mEntryDetail = parse(jsonString);
+            mEntryDetail = mParser.parse(jsonString);
             return Result.ok(mEntryDetail);
         } catch (Exception e) {
             return Result.ng(e);
@@ -79,33 +82,4 @@ public class EntryDetailLoader extends AsyncTaskLoader<Result<EntryDetail>> {
             return builder.toString();
         }
     }
-
-    @NonNull
-    private EntryDetail parse(String jsonString) throws JSONException {
-        JSONObject jsonObject = new JSONObject(jsonString);
-        return parseEntryDetailJson(jsonObject);
-    }
-
-    @NonNull
-    private EntryDetail parseEntryDetailJson(JSONObject jsonEntry) throws JSONException {
-        return EntryDetail.newBuilder()
-                .setDate(jsonEntry.getString("date"))
-                .setTitle(jsonEntry.getString("title"))
-                .setData(jsonEntry.getString("data"))
-                .setHtml(jsonEntry.getString("html"))
-                .setMinutes(jsonEntry.getInt("minutes"))
-                .setPubdate(jsonEntry.getString("pubdate"))
-                .setTags(parseTagsJson(jsonEntry.getJSONArray("tags")))
-                .build();
-    }
-
-    @NonNull
-    private List<String> parseTagsJson(JSONArray jsonTags) throws JSONException {
-        List<String> tags = new ArrayList<>();
-        for (int i = 0; i < jsonTags.length(); i++) {
-            tags.add(jsonTags.getString(i));
-        }
-        return tags;
-    }
-
 }
