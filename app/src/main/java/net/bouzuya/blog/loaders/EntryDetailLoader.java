67c23691c6b54ffa08fbe07984c49581a006f8ed
bouzuya <m@bouzuya.net>
2017-06-11 19:32:30 +0900

    extract EntryDetailRequest

--- app/src/main/java/net/bouzuya/blog/loaders/EntryDetailLoader.java
+++ app/src/main/java/net/bouzuya/blog/loaders/EntryDetailLoader.java
@@ -1,30 +1,18 @@
 package net.bouzuya.blog.loaders;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.support.v4.content.AsyncTaskLoader;
 
 import net.bouzuya.blog.models.EntryDetail;
 import net.bouzuya.blog.models.Result;
-import net.bouzuya.blog.parsers.EntryDetailParser;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import net.bouzuya.blog.requests.EntryDetailRequest;
 
 public class EntryDetailLoader extends AsyncTaskLoader<Result<EntryDetail>> {
     private final String mDateOrNull;
-    private final EntryDetailParser mParser;
-    private EntryDetail mEntryDetail;
 
     public EntryDetailLoader(Context context, String dateOrNull) {
         super(context);
         mDateOrNull = dateOrNull;
-        mParser = new EntryDetailParser();
     }
 
     @Override
@@ -36,44 +24,6 @@ public class EntryDetailLoader extends AsyncTaskLoader<Result<EntryDetail>> {
     @Override
     public Result<EntryDetail> loadInBackground() {
         if (mDateOrNull == null) throw new AssertionError();
-        try {
-            URL entryJsonUrl = newEntryJsonUrl(mDateOrNull);
-            String jsonString = fetch(entryJsonUrl);
-            mEntryDetail = mParser.parse(jsonString);
-            return Result.ok(mEntryDetail);
-        } catch (Exception e) {
-            return Result.ng(e);
-        }
-    }
-
-    @NonNull
-    private String fetch(URL url) throws IOException {
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-        try {
-            return readInputStream(urlConnection.getInputStream());
-        } finally {
-            urlConnection.disconnect();
-        }
-    }
-
-    @NonNull
-    private URL newEntryJsonUrl(String date) {
-        try {
-            return new URL("https://blog.bouzuya.net/" + date.replace('-', '/') + ".json");
-        } catch (MalformedURLException e) {
-            throw new AssertionError(e);
-        }
-    }
-
-    @NonNull
-    private String readInputStream(InputStream inputStream) throws IOException {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-            StringBuilder builder = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                builder.append(line);
-            }
-            return builder.toString();
-        }
+        return new EntryDetailRequest(mDateOrNull).send();
     }
 }
