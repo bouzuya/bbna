76f9ab4d9fa29c93114614129e4b03b5a95385cb
bouzuya <m@bouzuya.net>
2017-06-16 08:42:07 +0900

    add EntryRepository#get

--- app/src/main/java/net/bouzuya/blog/app/repository/EntryRepositoryImpl.java
+++ app/src/main/java/net/bouzuya/blog/app/repository/EntryRepositoryImpl.java
@@ -2,11 +2,18 @@ package net.bouzuya.blog.app.repository;
 
 
 import net.bouzuya.blog.app.request.EntryListRequest;
+import net.bouzuya.blog.domain.model.EntryDetail;
+import net.bouzuya.blog.domain.model.EntryId;
 import net.bouzuya.blog.domain.model.EntryList;
 import net.bouzuya.blog.domain.model.Result;
 import net.bouzuya.blog.domain.repository.EntryRepository;
 
 public class EntryRepositoryImpl implements EntryRepository {
+    @Override
+    public Result<EntryDetail> get(EntryId id) {
+        throw new UnsupportedOperationException();
+    }
+
     @Override
     public Result<EntryList> getAll() {
         return new EntryListRequest().send();
--- app/src/main/java/net/bouzuya/blog/domain/repository/EntryRepository.java
+++ app/src/main/java/net/bouzuya/blog/domain/repository/EntryRepository.java
@@ -1,8 +1,12 @@
 package net.bouzuya.blog.domain.repository;
 
+import net.bouzuya.blog.domain.model.EntryDetail;
+import net.bouzuya.blog.domain.model.EntryId;
 import net.bouzuya.blog.domain.model.EntryList;
 import net.bouzuya.blog.domain.model.Result;
 
 public interface EntryRepository {
+    Result<EntryDetail> get(EntryId id);
+
     Result<EntryList> getAll();
 }
