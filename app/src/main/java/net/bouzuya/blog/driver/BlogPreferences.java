d8ccb96fb5be5d299b626edb50309c6e1032008f
bouzuya <m@bouzuya.net>
2017-07-07 09:06:17 +0900

    add selected date to blog preferences

--- app/src/main/java/net/bouzuya/blog/driver/BlogPreferences.java
+++ app/src/main/java/net/bouzuya/blog/driver/BlogPreferences.java
@@ -8,6 +8,7 @@ import net.bouzuya.blog.entity.Optional;
 public class BlogPreferences {
     private static final String PREF_NAME = "blog.bouzuya.net";
     private static final String PREF_KEY_LATEST_DATE = "latest_date";
+    private static final String PREF_KEY_SELECTED_DATE = "selected_date";
 
     private final Context context;
 
@@ -20,9 +21,23 @@ public class BlogPreferences {
         return Optional.ofNullable(preferences.getString(PREF_KEY_LATEST_DATE, null));
     }
 
+    public Optional<String> getSelectedDate() {
+        SharedPreferences preferences = getPreferences();
+        return Optional.ofNullable(preferences.getString(PREF_KEY_SELECTED_DATE, null));
+    }
+
     public void setLatestDate(String latestDate) {
         SharedPreferences preferences = getPreferences();
         boolean isOk = preferences.edit().putString(PREF_KEY_LATEST_DATE, latestDate).commit();
+        if (!isOk) {
+            // TODO
+            throw new IllegalStateException();
+        }
+    }
+
+    public void setSelectedDate(String selectedDate) {
+        SharedPreferences preferences = getPreferences();
+        boolean isOk = preferences.edit().putString(PREF_KEY_SELECTED_DATE, selectedDate).commit();
         if (!isOk) {
             // TODO
             throw new IllegalStateException();
