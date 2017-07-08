2aca07e4d2f9c39ac1c734e99bcde5bc183a2728
bouzuya <m@bouzuya.net>
2017-07-08 19:35:43 +0900

    fix ResultTest

--- app/src/test/java/net/bouzuya/blog/entity/ResultTest.java
+++ app/src/test/java/net/bouzuya/blog/entity/ResultTest.java
@@ -3,37 +3,37 @@ package net.bouzuya.blog.entity;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class ResultTest {
     @Test
-    public void simpleOk() throws Exception {
+    public void testOk() throws Exception {
         Result<String> result = Result.ok("foo");
-        assertThat(result.isOk(), is(equalTo(true)));
+        assertThat(result.isOk(), is(true));
         try {
             //noinspection ThrowableResultOfMethodCallIgnored
             result.getException();
             Assert.fail();
         } catch (IllegalStateException e) {
-            assertThat(e, is(not(equalTo(null))));
+            assertThat(e, is(not(nullValue())));
         }
-        assertThat(result.getValue(), is(equalTo("foo")));
+        assertThat(result.getValue(), is("foo"));
     }
 
     @Test
-    public void simpleNg() throws Exception {
+    public void testNg() throws Exception {
         Result<String> result = Result.ng(new IllegalArgumentException("bar"));
-        assertThat(result.isOk(), is(equalTo(false)));
+        assertThat(result.isOk(), is(false));
         //noinspection ThrowableResultOfMethodCallIgnored
-        assertThat(result.getException().getMessage(), is(equalTo("bar")));
+        assertThat(result.getException().getMessage(), is("bar"));
         try {
             result.getValue();
             Assert.fail();
         } catch (IllegalStateException e) {
-            assertThat(e, is(not(equalTo(null))));
+            assertThat(e, is(not(nullValue())));
         }
     }
 }
