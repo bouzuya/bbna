97a86f3af99d96fce1cc9b0b262ab9ff32418efa
bouzuya <m@bouzuya.net>
2024-01-02 22:18:49 +0900

    Run `npm run format:write && npm run lint --fix`

--- frontend/src/components/Root.tsx
+++ frontend/src/components/Root.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { StatusBar } from "expo-status-bar";
+import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export function Root(): JSX.Element {
