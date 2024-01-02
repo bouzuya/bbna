667e0bcb4b1d432d89ecae684d15e99d7a149c2a
bouzuya <m@bouzuya.net>
2024-01-02 22:14:03 +0900

    Add typescript settings

---
+++ frontend/App.tsx
@@ -0,0 +1,4 @@
+import { Root } from "./src/components/Root";
+
+// eslint-disable-next-line import/no-default-export
+export default Root;
--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,6 +1,18 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@": "./src",
+          },
+          extensions: [".android.js", ".ios.js", ".json", ".js", ".ts", ".tsx"],
+          root: ["."],
+        },
+      ],
+    ],
   };
 };
