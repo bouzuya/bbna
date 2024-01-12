1fd5b42162d3d1ff60328f501d39e7a10188a1f0
bouzuya <m@bouzuya.net>
2024-01-12 10:53:38 +0900

    Extract handler mod

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,77 +1,7 @@
-use std::{collections::HashSet, sync::Arc};
+mod handler;
 
-use axum::{
-    extract::{Path, State},
-    http::StatusCode,
-    routing, Json, Router,
-};
-use expo_push_notification_client::{Expo, ExpoClientOptions, ExpoPushMessage};
-use tokio::{net::TcpListener, sync::Mutex};
-
-#[derive(Clone, Debug, Default)]
-struct App {
-    expo_push_tokens: Arc<Mutex<HashSet<String>>>,
-}
-
-#[derive(Debug, serde::Deserialize, serde::Serialize)]
-struct CreateExpoPushTokenRequestBody {
-    expo_push_token: String,
-}
-
-async fn create_expo_push_token(
-    State(App { expo_push_tokens }): State<App>,
-    Json(CreateExpoPushTokenRequestBody { expo_push_token }): Json<CreateExpoPushTokenRequestBody>,
-) -> StatusCode {
-    let mut expo_push_tokens = expo_push_tokens.lock().await;
-    // TODO: already exists
-    expo_push_tokens.insert(expo_push_token);
-    StatusCode::CREATED
-}
-
-async fn delete_expo_push_token(
-    State(App { expo_push_tokens }): State<App>,
-    Path(expo_push_token): Path<String>,
-) -> StatusCode {
-    let mut expo_push_tokens = expo_push_tokens.lock().await;
-    // TODO: not found
-    expo_push_tokens.remove(&expo_push_token);
-    StatusCode::NO_CONTENT
-}
-
-async fn create_notification(
-    State(App { expo_push_tokens }): State<App>,
-) -> Result<StatusCode, StatusCode> {
-    let expo_client = Expo::new(ExpoClientOptions {
-        // TODO
-        access_token: None,
-    });
-    let expo_push_tokens = expo_push_tokens.lock().await;
-    expo_client
-        .send_push_notifications(
-            ExpoPushMessage::builder(expo_push_tokens.clone())
-                .build()
-                .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?,
-        )
-        .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-    Ok(StatusCode::CREATED)
-}
-
-async fn get_root() -> &'static str {
-    "OK"
-}
-
-fn route(app: App) -> Router {
-    Router::new()
-        .route("/", routing::get(get_root))
-        .route("/expo_push_tokens", routing::post(create_expo_push_token))
-        .route(
-            "/expo_push_tokens/:expo_push_token",
-            routing::delete(delete_expo_push_token),
-        )
-        .route("/notifications", routing::post(create_notification))
-        .with_state(app)
-}
+use handler::{route, App};
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -85,10 +15,12 @@ mod tests {
     use axum::{
         async_trait,
         body::Body,
-        http::{header::CONTENT_TYPE, Request},
+        http::{header::CONTENT_TYPE, Request, StatusCode},
         response::Response,
     };
     use tower::ServiceExt;
+
+    use crate::handler::CreateExpoPushTokenRequestBody;
 
     use super::*;
 
