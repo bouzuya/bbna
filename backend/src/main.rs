1c26841607ade9c3515e93745f8dad44aec2e19b
bouzuya <m@bouzuya.net>
2024-02-14 22:30:01 +0900

    Add tests for bbn api

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -32,4 +32,58 @@ mod tests {
             )?)
         }
     }
+
+    #[tokio::test]
+    async fn test_bbn_api_entries() -> anyhow::Result<()> {
+        #[derive(Debug, PartialEq, serde::Deserialize)]
+        struct Entry {
+            date: String,
+            minutes: u16,
+            pubdate: String,
+            tags: Vec<String>,
+            title: String,
+        }
+        let response = reqwest::get("https://blog.bouzuya.net/posts.json").await?;
+        assert!(response.status().is_success());
+        let response_body = response.text().await?;
+        let entries = serde_json::from_str::<'_, Vec<Entry>>(response_body.as_str())?;
+        assert!(entries.contains(&Entry {
+            date: "2012-04-08".to_string(),
+            minutes: 35,
+            pubdate: "2012-04-08T21:12:21+09:00".to_string(),
+            tags: vec!["misc".to_string()],
+            title: "じごくのそうべえ".to_string(),
+        }));
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_bbn_api_entry() -> anyhow::Result<()> {
+        #[derive(Debug, PartialEq, serde::Deserialize)]
+        struct EntryDetail {
+            data: String,
+            date: String,
+            minutes: u16,
+            html: String,
+            pubdate: String,
+            tags: Vec<String>,
+            title: String,
+        }
+        let response = reqwest::get("https://blog.bouzuya.net/2012/04/08.json").await?;
+        assert!(response.status().is_success());
+        let response_body = response.text().await?;
+        let entry = serde_json::from_str::<'_, EntryDetail>(response_body.as_str())?;
+        assert!(entry
+            .data
+            .starts_with("金曜日の夜が天国なら日曜日の夜は地獄である。"));
+        assert_eq!(entry.date, "2012-04-08");
+        assert_eq!(entry.minutes, 35);
+        assert!(entry
+            .html
+            .starts_with("<p>金曜日の夜が天国なら日曜日の夜は地獄である。</p>"));
+        assert_eq!(entry.pubdate, "2012-04-08T21:12:21+09:00");
+        assert_eq!(entry.tags, vec!["misc".to_string()]);
+        assert_eq!(entry.title, "じごくのそうべえ");
+        Ok(())
+    }
 }
